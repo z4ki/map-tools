d0a50fd2732890381afb4187c14a52178c457072
Zak Rovsky <z4ki.xy@gmail.com>
2017-03-22 20:00:37 +0100

    log modified config/app.php commented code

--- config/app.php
+++ config/app.php
@@ -119,8 +119,8 @@ return [
     | Available Settings: "single", "daily", "syslog", "errorlog"
     |
     */
-
-    'log' => env('APP_LOG', 'single'),
+    'log' => 'errorlog',
+    /*'log' => env('APP_LOG', 'single'),*/
 
     'log_level' => env('APP_LOG_LEVEL', 'debug'),
 
